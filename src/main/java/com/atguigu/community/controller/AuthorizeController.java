475a9e585f5eec8e0dc00c9ea073c03f29e6e256
wujie <1531548541@qq.com>
2020-11-07 15:12:07 +0800

    github社交登录(未将数据保存进数据库)

---
+++ src/main/java/com/atguigu/community/controller/AuthorizeController.java
@@ -0,0 +1,52 @@
+package com.atguigu.community.controller;
+
+import com.atguigu.community.dto.AccessTokenDTO;
+import com.atguigu.community.dto.GithubUser;
+import com.atguigu.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * github社交登录
+ */
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @Value("${github.client.id}")
+    private String clientId;
+
+    @Value("${github.client.secret}")
+    private String clientSecret;
+
+    @Value("${github.redirect.uri}")
+    private String redirectUri;
+
+
+    @RequestMapping("/callback")
+    public String callback(@RequestParam String code,
+                           @RequestParam String state,
+                           HttpServletRequest request){
+        AccessTokenDTO accessTokenDTO=new AccessTokenDTO();
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        if (user != null) {
+            //存入session
+            request.getSession().setAttribute("user",user);
+            return "redirect:/";
+        }
+        return "redirect:/";
+    }
+}
--- src/test/java/com/atguigu/community/CommunityApplicationTests.java
+++ src/test/java/com/atguigu/community/CommunityApplicationTests.java
@@ -3,11 +3,33 @@ package com.atguigu.community;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+
 @SpringBootTest
 class CommunityApplicationTests {
 
     @Test
     void contextLoads() {
+        int[] T={73, 74, 75, 71, 69, 72, 76, 73};
+        System.out.println(Arrays.toString(dailyTemperatures(T)));
+    }
+
+    @Test
+    public int[] dailyTemperatures(int[] T) {
+         int[] ans = new int[T.length];
+//         Stack<Integer> stack = new Stack<Integer>();
+        Deque<Integer> stack = new LinkedList<Integer>();
+         for (int i = 0; i < T.length; i++) {
+             while (!stack.isEmpty() && T[i] > T[stack.peek()]) {
+                 int prevIndex = stack.pop();
+                 ans[prevIndex] = i - prevIndex;
+             }
+             stack.push(i);
+         }
+         return ans;
     }
 
 }
